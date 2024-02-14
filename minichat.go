c5d9bdb0cc7c75bf26bcbe1a933190a6dd55b9ec
hanke0 <hanke0@outlook.com>
2024-02-14 14:18:56 +0800

    update v0.1.7

--- minichat.go
+++ minichat.go
@@ -562,7 +562,7 @@ func main() {
 	version := flag.Bool("version", false, "show version")
 	flag.Parse()
 	if *version {
-		fmt.Println("minichat v0.1.6")
+		fmt.Println("minichat v0.1.7")
 		return
 	}
 	if v := os.Getenv("ADDR"); v != "" && *addr == "" {
